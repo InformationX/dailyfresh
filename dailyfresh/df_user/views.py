cb3e0529af9ecf670470762696f4adb19cc1fadd
JavsShao <javs_shao@163.com>
2019-06-17 16:03:16 +0800

    注册视图前后端实现

--- dailyfresh/df_user/views.py
+++ dailyfresh/df_user/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from hashlib import sha1
 
 # Create your views here.
+from df_user.models import *
+
+
+def register(request):
+    return render(request, 'df_user/register.html')
+
+def register_handle(request):
+    # 接收用户参数
+    post = request.POST
+    user_name = post.get('user_name')
+    password = post.get('pwd')
+    password2 = post.get('cpwd')
+    email = post.get('email')
+    # 对照两次密码是否相同
+    if password != password2:
+        return render(request, 'df_user/register.html')
+    # 加密密码
+    s1 = sha1()
+    s1.update(password.encode('utf8'))
+    password3 = s1.hexdigest()
+    # 创建对象，保存数据到数据库中
+    user = UserInfo()
+    user.user_name = user_name
+    user.password = password3
+    user.email = email
+    user.save()
+
+    # 注册成功, 转到登录页面
+    return redirect('login')
